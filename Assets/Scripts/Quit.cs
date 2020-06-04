f261abb887c2d50e9b275a5040765edb49dde934
AngelaDrous <65650448+AngelaDrous@users.noreply.github.com>
2020-06-04 20:07:42 +0300

    Update

---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+    [SerializeField]
+    KeyCode keyEscape;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keyEscape))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
---
+++ Assets/Scripts/RestartLevelOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevelOnCollision : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
+
